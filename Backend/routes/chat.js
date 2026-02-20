5177f3a4c1895eb303e3476030e24ba8939a64e6
sachin624k <sachin77000k@gmail.com>
2026-02-20 12:22:25 +0530

    backend chat system completed

---
+++ Backend/routes/chat.js
@@ -0,0 +1,107 @@
+import express from "express";
+import Thread from "../models/Thread.js";
+import getOpenAIAPIResponse from "../utils/openai.js";
+
+const router = express.Router();
+
+//Test
+router.post("/test", async(req, res) => {
+    try {
+        const thread = new Thread({
+            threadId: "xyz",
+            title: "Testing new Thread"
+        });
+
+        const response = await thread.save();
+        res.send(response);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error: "Failed to save in DB"});
+    }
+});
+
+// Get all threads
+router.get("/thread", async(req, res) => {
+    try {
+        const threads = await Thread.find({}).sort({updatedAt: -1});
+        //Descending order of updatedAt...most recent data on top
+        res.json(threads);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error: "Failed to fetch threads"});
+    }
+});
+
+//Get thread on the basis of ID
+router.get("/thread/:threadId", async(req, res) => {
+    const {threadId} = req.params;
+
+    try {
+        const thread = await Thread.findOne({threadId});
+        
+        if(!thread){
+            res.status(404).json({error: "Thread not found"});
+        }
+
+        res.json(thread.messages);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error: "Failed to fetch chat"});
+    }
+});
+
+//Delete a thread on the basis of ID
+router.delete("/thread/:threadId", async(req, res) => {
+    const {threadId} = req.params;
+
+    try {
+        const deletedThread = await Thread.findOneAndDelete({threadId});
+        
+        if(!deletedThread){
+            res.status(404).json({error: "Thread not found"});
+        }
+
+        res.status(200).json({success: "Thread deleted successfully"});
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error: "Failed to delete thread"});
+    }
+})
+
+// Send message → get AI reply → save both → return reply
+router.post("/chat", async(req, res) => {
+    const {threadId, message} = req.body;
+
+    if(!threadId || !message){
+        res.status(400).json({error: "missing required filled"});
+    }
+    try {
+        const thread = await Thread.findOne({threadId});
+
+        if(!threadId){
+            //create a new thread in DB
+            thread = new Thread({
+                threadId,
+                title: message,
+                messages: [{role: "user", content: message}]
+            });
+        } else{
+            thread.messages.push({role: "user", content: message});
+        }
+
+        const assistantReply = await getOpenAIAPIResponse(message);
+        
+        thread.messages.push({role: "assistant", content: assistantReply});
+        thread.updatedAt = new Date();
+
+        await thread.save();
+        res.json({reply: assistantReply});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error: "something want wrong"});
+    }
+});
+
+export default router;
--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import "dotenv/config"
 import cors from "cors";
+import mongoose from "mongoose";
+import chatRoutes from "./routes/chat.js";
 
 const app = express();
 const PORT = 8080;
@@ -8,32 +10,44 @@ const PORT = 8080;
 app.use(express.json());
 app.use(cors());
 
+app.use("/api", chatRoutes);
+
 app.listen(PORT, () => {
   console.log(`server is running on ${PORT}`);
+  connectDB();
 });
 
-app.post("/test", async(req, res) => {
-  const options = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${process.env.GROQ_API_KEY}`
-    },
-    body: JSON.stringify({
-      model: "llama-3.1-8b-instant",
-      messages: [{
-        role: "user",
-        content: req.body.message
-      }]
-    })
-  };
-
+const connectDB = async() => {
   try {
-    const response = await fetch("https://api.groq.com/openai/v1/chat/completions", options);
-    const data = await response.json();
-    console.log(data.choices[0].message.content);
-    res.send(data.choices[0].message.content);
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected with Database!");
   } catch (error) {
-    console.log(error)
+    console.log("Failed to connect with DB!", error);
   }
-});
+}
+
+// app.post("/test", async(req, res) => {
+//   const options = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Authorization": `Bearer ${process.env.GROQ_API_KEY}`
+//     },
+//     body: JSON.stringify({
+//       model: "llama-3.1-8b-instant",
+//       messages: [{
+//         role: "user",
+//         content: req.body.message
+//       }]
+//     })
+//   };
+
+//   try {
+//     const response = await fetch("https://api.groq.com/openai/v1/chat/completions", options);
+//     const data = await response.json();
+//     // console.log(data.choices[0].message.content);
+//     res.send(data.choices[0].message.content);
+//   } catch (error) {
+//     console.log(error)
+//   }
+// });
