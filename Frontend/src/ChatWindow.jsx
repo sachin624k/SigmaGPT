64c0735a7aeae182cd1e9000a26c498874b313a2
sachin624k <sachin77000k@gmail.com>
2026-02-21 12:53:29 +0530

    Added ChatWindow

--- Frontend/src/ChatWindow.jsx
+++ Frontend/src/ChatWindow.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
 import "./ChatWindow.css";
+import Chat from "./Chat.jsx";
 
 const ChatWindow = () => {
   return (
-    <div>
-      ChatWindow
+    <div className='chatWindow'>
+      <div className='navbar'>
+        <span>SigmaGPT <i class="fa-solid fa-angle-down"></i></span>
+        <div className="userIconDiv">
+          <span className="userIcon"><i class="fa-solid fa-user"></i></span>
+        </div>
+      </div>
+      {/* <Chat></Chat> */}
+
+      <div className="chatInput">
+        <div className="inputBox">
+          <input placeholder="Ask anything">
+
+          </input>
+          <div id="submit"><i class="fa fa-paper-plane" aria-hidden="true"></i></div>
+        </div>
+        <p className="info">
+          SigmaGPT can make mistakes. Check important info. See Cookie Preferences.
+        </p>
+      </div>
     </div>
   )
 }
